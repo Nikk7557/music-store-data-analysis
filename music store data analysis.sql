8bf4f979d4527a2813b0dd75b6aff9aa2738aaf0
Nikhil Chaudhary <167134638+Nikk7557@users.noreply.github.com>
2025-11-20 16:42:02 +0530

    Add files via upload

---
+++ music store data analysis.sql
@@ -0,0 +1,65 @@
+use music;
+
+-- Who is the best customer? The customer who has spent the most money will be declared the best customer. 
+-- Write a query that returns the person who has spent the most money
+
+select customer.customer_id , customer.last_name ,customer.first_name , sum(invoice.total) as total_spe 
+from customer 
+join invoice on customer.customer_id = invoice.customer_id  
+GROUP BY customer.customer_id , customer.customer_id , customer.last_name ,customer.first_name
+order by total_spe desc limit 1;
+-- Write query to return the email, first name, last name, & Genre of all Rock Music listeners. 
+-- Return your list ordered alphabetically by email starting with A
+
+select distinct email,last_name ,first_name  from customer 
+join invoice on customer.customer_id = invoice.customer_id
+join invoice_line on invoice.invoice_id = invoice_line.invoice_id 
+where track_id in (select track_id from track
+join genre on track.genre_id = genre.genre_id 
+where genre.name like "rock")
+order by email;
+
+-- Let's invite the artists who have written the most rock music in our dataset. 
+-- Write a query that returns the Artist name and total track count of the top 10 rock bands
+
+select artist.artist_id, artist.name , count(artist.artist_id) as no_of_songs 
+from track 
+join album1 on album1.album_id = track.album_id 
+join artist on artist.artist_id = album1.artist_id
+join genre on genre.genre_id = track.genre_id 
+where genre.name like "rock" 
+group by artist.artist_id ,artist.name ,artist.artist_id
+order by no_of_songs desc limit 10;
+
+--  Return all the track names that have a song length longer than the average song length. 
+-- Return the Name and Milliseconds for each track. Order by the song length with the longest songs listed first.
+
+select name,milliseconds from track 
+where milliseconds > ( select avg(milliseconds) as avg_track_length from track)
+order by milliseconds desc limit 5;
+
+-- who is the most senior employee on job title 
+
+select title,first_name,last_name from employee 
+order by levels 
+desc limit 5;
+
+-- which country have most invoices
+
+select count(*)  as total , billing_country from invoice
+group by billing_country
+order by total desc limit 5;
+
+-- What are top 3 values of total invoice?
+
+select total from invoice 
+order by total desc limit 3;
+
+-- Which city has the best customers? We would like to throw a promotional Music Festival in the city 
+-- we made the most money. Write a query that returns one city that has the highest sum of invoice totals. 
+-- Return both the city name & sum of all invoice totals
+
+select billing_city , sum(total) as total_invoice
+from invoice
+group by billing_city
+order by total_invoice desc limit 5;
